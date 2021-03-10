49ae72b1b80f7eec4c3ef65bda73e998016a18f6
Rebekah Kulidzan <rkulidzan@users.noreply.github.com>
2021-03-10 09:40:13 +0000

    Update magic-8.py

--- magic-8.py
+++ magic-8.py
@@ -1,12 +1,13 @@
 import random
 import time, sys
+
 play_again = 'yes'
 
 while play_again == 'yes':
-    str(input("Welcome to the Magic 8-Ball. Enter your question: ")).lower()
-    
-#Delay output for 1 second each
-    print("Thinking...")
+    str(input("Welcome to your shady Magic 8-Ball. Enter your question: ")).lower()
+
+    # Delay output for 1 second each
+    print("Wait there while I get my tea...")
     time.sleep(1)
     print("3...")
     time.sleep(1)
@@ -16,17 +17,17 @@ while play_again == 'yes':
     time.sleep(1)
     print()
 
-#Generate a random integer/response
-    response = random.randint(0,8)
-    
+    # Generate a random integer/response
+    response = random.randint(0, 8)
+
     if response == 1:
         print("LOL. No chance!")
     elif response == 2:
-        print("Maybe. See how I feel later")
+        print("Maybe. See how I feel later.")
     elif response == 3:
         print("Ask Ricky.")
     elif response == 4:
-        print("Sure!")
+        print("Sure... alright then.")
     elif response == 5:
         print("Yes, it will rain tomorrow.")
     elif response == 6:
@@ -36,11 +37,9 @@ while play_again == 'yes':
     elif response == 8:
         print("I always knew you were a winner.")
     else:
-        print("Invalid question. Try again!")
+        print("Boring question. Try again!")
 
-    play_again = str(input("Would you like to ask another question? yes/no/enter ")).lower()
+    play_again = str(input("Would you like to ask another question? yes/no ")).lower()
     if play_again == 'no':
-        print("Goodbye! Thanks for playing!")
-        sys.exit()
-    elif: play_again == "":
+        print("Bye, Felicia.")
         sys.exit()
